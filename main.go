d5c4084164c89ea04da1f75bb596e4aedbca28fa
shankalpaLamichhane <shankalpa.lamichhane@gmail.com>
2021-11-11 20:32:02 +0545

    port mapping

--- main.go
+++ main.go
@@ -16,5 +16,5 @@ func main() {
     // http.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
     //     fmt.Fprint(rw, "This is mycodesmells/golang-examples server from Heroku!")
     // })
-		http.ListenAndServe(":", os.Getenv("PORT"))
+		http.ListenAndServe(":"+os.Getenv("PORT"),router)
 }
